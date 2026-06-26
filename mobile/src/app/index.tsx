4ebdb6bf554927f2a97ff8cc60a764cdb167430d
KofiIsaacBoateng <kofiisaacboateng@gmail.com>
2026-06-26 02:22:32 +0000

    mobile: google services update

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, View } from "react-native";
 export default function EntryPoint() {
   return (
     <View style={styles.container}>
-      <Redirect href="/userrole" />
+      <Redirect href="/onboarding" />
     </View>
   );
 }
