39921e90674f30dbcfec1896159bd8e3f44544a0
KofiIsaacBoateng <kofiisaacboateng@gmail.com>
2026-08-17 23:47:25 +0000

    backend/hotfix(payment-requests)

--- backend/src/modules/pay-r/presentation/controllers/payment-request.controller.ts
+++ backend/src/modules/pay-r/presentation/controllers/payment-request.controller.ts
@@ -3,6 +3,7 @@ import type { NextFunction, Request, Response } from "express";
 import { CreatePaymentRequestUseCase } from "../../application/use-cases/create-payment-request.usecase.js";
 
 import { toPaymentRequestResponse } from "../../application/dto/payment-request.dto.js";
+import { sendSuccess } from "@/shared/presentation/utils/response-formatter.js";
 
 export class PaymentRequestController {
   constructor(
@@ -10,18 +11,16 @@ export class PaymentRequestController {
   ) {}
 
   async create(req: Request, res: Response, next: NextFunction): Promise<void> {
-    try {
-      const request = await this.createPaymentRequestUseCase.execute(
-        req.authUser!.userId,
-        req.body,
-      );
+    const request = await this.createPaymentRequestUseCase.execute(
+      req.authUser.userId,
+      req.body,
+    );
 
-      res.status(201).json({
-        success: true,
-        data: toPaymentRequestResponse(request),
-      });
-    } catch (error) {
-      next(error);
-    }
+    sendSuccess(
+      res,
+      toPaymentRequestResponse(request),
+      "Payment request created!",
+      201,
+    );
   }
 }
